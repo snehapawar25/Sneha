3df0ef1d23ea9e8c98265cc0bd4664c50405c4fb
snehapawar25 <snehapawar201819@gmail.com>
2026-03-14 13:46:53 +0530

    Create BinaryPattern.java
    Binary Pattern program

---
+++ BinaryPattern.java
@@ -0,0 +1,22 @@
+//Binary Pattern Program
+public class BinaryPattern
+  {
+    public static void main(String[] args)
+    {
+      int num=6;
+      for(int i=1;i<=num;i++)
+        {
+          for(int j=1;j<=i;j++)
+            {
+              if((i+j) % 2 ==0)
+              {
+                System.out.print("1 ");
+              }
+              else{
+                System.out.print("0 ");
+              }
+            }
+          System.out.println();
+        }
+    }
+  }
